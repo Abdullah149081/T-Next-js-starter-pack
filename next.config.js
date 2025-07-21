46b07f9337955fe87edae96133c6aa8a3f64f577
Abdullah149081 <abdullah149081@hotmail.com>
2025-07-21 23:07:20 +0600

    chore: add pnpm workspace configuration and ignore specific built dependencies
    style: remove unnecessary newlines in base.css

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    // dev backend url
+    API_URL: "http://localhost:5000/api/v1",
+
+    // live backend url
+    // API_URL: "",
+
+    // frontend live url
+    FRONTEND_URL: "",
+  },
+
   images: {
     remotePatterns: [
       {
@@ -8,6 +19,17 @@ const nextConfig = {
       },
     ],
   },
+
+  // Optimize build performance
+  poweredByHeader: false,
+  compress: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
